ff70834b698a80e19fd17bd9c3da304b5bcf1803
Gabriel Caetano Farias <gabrielcfarias77@gmail.com>
2023-11-18 16:44:52 -0300

    esboço do codigo

--- projeto/main.c
+++ projeto/main.c
@@ -5,3 +5,22 @@
 #include "keyboard.h"
 #include "timer.h"
 
+//struct para coordenada e pontuação do player
+
+/*ideia para movimentação do player:
+-> inicialmente, printa a matriz com o ponto presente no quadrado 0x0
+-> caso o jogador clique para se mover, limpa terminal e printa denovo o ponto na outra coordenada
+*/
+
+/*funcao para printar o cenario:
+-> cenario consiste em uma matriz desenhada
+-> utilizar de loops
+
+exemplo:
+ __ __ __ ___
+|__|__|__|__|
+|__|__|__|__|
+|__|__|__|__|
+
+porem, em larga escala. (talvez exista algum codigo na internet que printe tabelas em C)
+*/
